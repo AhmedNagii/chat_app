60e42d9b8dbb2e16ad7b19b41225f482eb9bd837
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2021-03-07 00:45:37 +0200

    authScreen design added

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:chat_app/Screens/auth_Screen.dart';
+import 'package:flutter/material.dart';
+
+import './Screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          backgroundColor: Colors.pink,
+          accentColor: Colors.deepPurple,
+          accentColorBrightness: Brightness.dark,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.pink,
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)))),
+      home: AuthScreen(),
+    );
+  }
+}
