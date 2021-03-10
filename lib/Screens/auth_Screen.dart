0ea28e7af633d7429e5d3913291dbcb684966898
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2021-03-10 13:43:27 +0200

    signup error

--- lib/Screens/auth_Screen.dart
+++ lib/Screens/auth_Screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/services.dart';
+
 import '../widgets/auth_form.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -7,12 +10,41 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
   void _submitAuthForm(
     String email,
     String password,
     String username,
     bool isLogin,
-  ) {}
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        authResult = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } on PlatformException catch (error) {
+      var message = "An error occurrred, please check your credenrials ";
+      if (error.message != null) {
+        message = error.message;
+      }
+      final snackBar = SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(ctx).errorColor,
+      );
+      ScaffoldMessenger.of(ctx).showSnackBar(snackBar);
+    } catch (error) {
+      print(error);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -8,6 +8,7 @@ class AuthForm extends StatefulWidget {
     String password,
     String username,
     bool isLogin,
+    BuildContext ctx,
   ) submitFn;
 
   @override
@@ -28,10 +29,11 @@ class _AuthFormState extends State<AuthForm> {
     if (isValid) {
       _formKey.currentState.save();
       widget.submitFn(
-        _userEmail,
-        _userPassword,
-        _userName,
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
         _isLogin,
+        context,
       );
     }
   }
